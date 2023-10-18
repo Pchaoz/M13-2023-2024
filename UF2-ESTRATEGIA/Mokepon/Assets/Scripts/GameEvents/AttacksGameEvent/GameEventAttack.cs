78287f3dcbac27e764358fc4d1482ebd67eb99f3
Pchaozz <polsh2001.1@gmail.com>
2023-10-18 18:09:39 +0200

    Version 0.1 - GameEvents Pol
    - Created attack game events where the enemies recive a AttackInfo ( Scriptable Object with the attack info)

---
+++ UF2-ESTRATEGIA/Mokepon/Assets/Scripts/GameEvents/AttacksGameEvent/GameEventAttack.cs
@@ -0,0 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEventAttack", menuName = "GameEvent/GameEvent - GameEventAttack")]
+public class GameEventAttack : GameEvent<AttackInfo> { }
