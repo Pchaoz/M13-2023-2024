767ff7df9e60d81bea93f37e3bb7a5ed0a8c3e16
Pchaozz <polsh2001.1@gmail.com>
2023-11-06 21:20:43 +0100

    temp version 1.0
    - Tecnically the rng combat works

--- UF2-ESTRATEGIA/Mokepon/Assets/Scripts/Mokepon.cs
+++ UF2-ESTRATEGIA/Mokepon/Assets/Scripts/Mokepon.cs
@@ -11,18 +11,22 @@ public class Mokepon : MonoBehaviour
     [SerializeField]
     private int m_Hp;
     [SerializeField]
-    private List<AttackInfo> m_AttacksList;
+    public List<AttackInfo> m_AttacksList;
     [SerializeField]
     private string m_Type;
     [SerializeField]
     private string m_State;
 
-    void LoadInfo(MokeponInfo info)
+    public void LoadInfo(MokeponInfo info)
     {
         m_Name = info.mokename;
         m_Hp = info.hp;
         m_AttacksList = info.attackList;
         m_Type = info.type;
         m_State = info.state;
+    }
+    public void RecibeAttack(AttackInfo atk)
+    {
+        Debug.Log("AU JODER HE RECIBIDO:" + atk.damage);
     }
 }
