9b051603f6a797b0f6dfa6ae76dc77ec772379ee
raulmartincaro <raul.martincaro@gmail.com>
2023-11-20 17:06:37 +0100

    invulnerabilidad al entrar y salir de la hierba entre escenas
    .

--- UF2-ESTRATEGIA/Mokepon/Assets/Scripts/OpenWorld/WorldGameManager.cs
+++ UF2-ESTRATEGIA/Mokepon/Assets/Scripts/OpenWorld/WorldGameManager.cs
@@ -1,24 +1,33 @@
 using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WorldGameManager : MonoBehaviour
 {
     private IEnumerator m_pokemonHierba;
+    private IEnumerator m_segundosInvulnerable;
     [SerializeField]
     GameEventBoolean m_comienzoCombate;
     [SerializeField]
     JugadorController m_jugador1;
     [SerializeField]
     JugadorController m_jugador2;
+    [SerializeField]
     int m_jugadoresTocandoHierba;
+    [SerializeField]
+    bool m_Atacable;
 
     private void Awake()
     {
+        m_Atacable = false;
         m_pokemonHierba = posiblePokemonHierba();
+        m_segundosInvulnerable = invulnerabilidad();
         m_jugador1.OnpisandoHierba += JugadorHierba;
         m_jugador2.OnpisandoHierba += JugadorHierba;
         m_jugadoresTocandoHierba = 0;
+        StartCoroutine(m_segundosInvulnerable);
+       
     }
     private void OnDisable()
     {
@@ -27,23 +36,27 @@ public class WorldGameManager : MonoBehaviour
     }
 
     public void JugadorHierba(bool b)
-    {
+    { 
         if (b)
         {
-            if (m_jugadoresTocandoHierba ==0)
+            if (m_jugadoresTocandoHierba == 0)
             {
                 m_jugadoresTocandoHierba++;
+                if(m_Atacable)
                 StartCoroutine(m_pokemonHierba);
             }
+           
         }
         else
         {
             m_jugadoresTocandoHierba--;
             if (m_jugadoresTocandoHierba == 0)
             {
+                if (m_Atacable)
                 StopCoroutine(m_pokemonHierba);
-            }               
-        }      
+            }
+        }
+
     }
 
 
@@ -64,6 +77,20 @@ public class WorldGameManager : MonoBehaviour
             }
             yield return new WaitForSeconds(1);
         }
+       
+    }
+
+    IEnumerator invulnerabilidad()
+    {
+        Debug.Log("No me pueden atacar");
+        yield return new WaitForSeconds(3);
+        Debug.Log("Me pueden atacar");
+        m_Atacable = true;
+        if (m_jugadoresTocandoHierba != 0)
+        {
+            StartCoroutine(m_pokemonHierba);
+        }
+        yield return null;
     }
 
    
